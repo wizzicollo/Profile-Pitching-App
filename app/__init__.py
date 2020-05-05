4c4ff3e544fe46aef8788385aafe9219bf0bf9f4
Frozen Feelings <kiprutohcollo@gmail.com>
2020-05-06 00:31:30 +0300

    created init.py

---
+++ app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+from sqlalchemy import create_engine
+
+
+simple = SimpleMDE()
+login_manager = LoginManager()
+login_manager._session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
+create_engine = create_engine('postgresql+psycopg2://root:123456@localhost/pitchit')
+
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
+    app.config.from_object(config_options[config_name])
+    configure_uploads(app, photos)
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+    
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
